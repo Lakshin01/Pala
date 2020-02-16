6e642d9a363d65d7762e1139b9f2a10ae67ad04e
Alrin <alrinajabin@gmail.com>
2020-02-17 02:57:44 +0530

    hi

--- client/src/components/service/Search.js
+++ client/src/components/service/Search.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class Search extends Component {
-    render() {
-        return (
-            <div classNameName="ui container">
-                                <div className="ui fluid category search">
-                <div className="ui icon input">
-                    <input className="ui segment" type="text" 
-                    placeholder="Search  ..."/>
-                    <i className="search icon"></i>
-                </div>
-                <div className="results">
-                    <li>Hello</li>
-                </div>
-                </div>
-            </div>
-        )
-    }
+class Search extends React.Component {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term)
+    // this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-export default Search;
+
+Search.propTypes = {
+    place: PropTypes.object.isRequired
+}
+
+
+const mapStateToProps = state => ({
+    place: state.service.place,
+    errors: state.errors
+  });
+  
+
+export default  connect(mapStateToProps)(Search);
